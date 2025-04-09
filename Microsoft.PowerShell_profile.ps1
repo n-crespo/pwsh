e991ee181d55db5703881138a09a5fb47a165071
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-04-09 15:36:37 -0700

    fix: only use psreadline in interactive sessions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ function Exit-Shell { exit }
 Set-Alias -Name q -Value Exit-Shell
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name n -Value nvim
+Set-Alias -Name g -Value git
 Function n. { nvim . }
 Function nl {nvim -c':e#<1'}
 Function e { yazi }
@@ -23,36 +24,41 @@ Function targetstop {
 
 # other
 # Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-Set-Alias -Name so -Value refreshenv
+  Set-Alias -Name so -Value refreshenv
 
-Import-Module PSReadLine
 # $ENV:EDITOR = "C:\Program Files\Neovim\bin\nvim.exe"
-$ENV:EDITOR = "\\wsl$\Ubuntu\usr\bin\nvim"
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle List
+  $ENV:EDITOR = "\\wsl$\Ubuntu\usr\bin\nvim"
 
-Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-Set-PSReadlineKeyHandler -Key ctrl+enter -Function AcceptLine
-Set-PSReadlineKeyHandler -Key ctrl+n -Function NextHistory
-Set-PSReadlineKeyHandler -Key ctrl+p -Function PreviousHistory
-Set-PSReadlineOption -BellStyle None
-Set-PSReadlineKeyHandler -Key ctrl+l -Function ClearScreen
+  $env:TERM='xterm-256color'
+  $env:PATH += ";C:\Users\nicol\sqlite\"
 
-$env:TERM='xterm-256color'
-$env:PATH += ";C:\Users\nicol\sqlite\"
+  function lis { eza --icons=always --group-directories-first @args }
+  Set-Alias ls lis
 
-function lis { eza --icons=always --group-directories-first @args }
-Set-Alias ls lis
+  function laa { ls -Alh --group-directories-first @args }
+  Set-Alias la laa
 
-function laa { ls -Alh --group-directories-first @args }
-Set-Alias la laa
+  function ll { ls -l --group-directories-first --git -a @args }
+  Set-Alias l ll
 
-function ll { ls -l --group-directories-first --git -a @args }
-Set-Alias l ll
+  function ltt { ls --tree @args }
+  Set-Alias lt ltt
 
-function ltt { ls --tree @args }
-Set-Alias lt ltt
-
-Set-Alias ff fastfetch
+  Set-Alias ff fastfetch
 # Clear-Host
+
+# Only run in interactive shell
+if ($Host.Name -eq 'ConsoleHost') {
+  Import-Module PSReadLine
+  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+  Set-PSReadLineOption -PredictionViewStyle List
+
+  Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+  Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+  Set-PSReadlineKeyHandler -Key ctrl+enter -Function AcceptLine
+  Set-PSReadlineKeyHandler -Key ctrl+n -Function NextHistory
+  Set-PSReadlineKeyHandler -Key ctrl+p -Function PreviousHistory
+  Set-PSReadlineOption -BellStyle None
+  Set-PSReadlineKeyHandler -Key ctrl+l -Function ClearScreen
+}
+
