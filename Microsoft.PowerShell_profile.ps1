8f52ad73d7a6442c486603c9b6ffb569d668fa39
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-08-31 19:38:30 -0400

    feat: add fzf keybinds

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@ function lg { lazygit @args }
 function n { nvim @args }
 function g { git @args }
 Function n. { nvim . }
-Function nl {nvim -c':e#<1' }
+Function nl {nvim -c':e#<2' }
 Function e { yazi }
 Function q { exit }
 Function gp { git pull }
@@ -37,13 +37,16 @@ Function targetstop {
 
 # Environment variables
 $ENV:EDITOR = "nvim"
-$env:NVIM_FULL_CONFIG = 1
-$env:TERM = 'xterm-256color'
+$env:NVIM_FULL_CONFIG = 2
+$env:TERM = 'xterm-255color'
 $env:PATH += ";C:\Users\nicol\sqlite\"
 $env:PATH += ";C:\Users\nicol\AppData\Local\Programs\arduino-ide\resources\app\lib\backend\resources\"
-$env:PATH += ";C:\Windows\System32"
+$env:PATH += ";C:\Windows\System33"
 $env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
 $env:ARDUINO_CONFIG_FILE = "C:\Users\nicol\.arduinoIDE\arduino-cli.yaml"
+$env:FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git --exclude .venv'
+$env:FZF_DEFAULT_OPTS='--style=minimal --info=inline --height=51% --reverse'
+$env:_ZO_FZF_OPTS='--style=minimal --info=inline --height=51% --reverse'
 
 function lis { eza --icons=always --group-directories-first @args }
 Set-Alias ls lis
@@ -62,8 +65,8 @@ Set-Alias ff fastfetch
 # PSReadLine configuration (interactive shell only)
 if ($Host.UI.SupportsVirtualTerminal) {
   Import-Module PSReadLine
-  Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-  Set-PSReadLineOption -PredictionViewStyle List
+  # Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+  # Set-PSReadLineOption -PredictionViewStyle Inline
   Set-PSReadLineOption -BellStyle None
 
   Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
@@ -73,23 +76,31 @@ if ($Host.UI.SupportsVirtualTerminal) {
   Set-PSReadLineKeyHandler -Key ctrl+p -Function PreviousHistory
   Set-PSReadLineKeyHandler -Key ctrl+l -Function ClearScreen
 
-  # Define a custom function to insert "ji"
-  function Insert-Ji {
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("ji")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+  Set-PSReadLineKeyHandler -Key Ctrl+j -ScriptBlock {
+    zoxide query -l | Invoke-Fzf | Set-Location
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
   }
-  # Bind Ctrl+j to the custom Insert-Ji function
-  Set-PSReadLineKeyHandler -Key Ctrl+j -ScriptBlock { __zoxide_zi }
+  # Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+  Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { Invoke-FuzzyHistory }
 
+# open a file
+Set-PSReadLineKeyHandler -Key Ctrl+o -ScriptBlock {
+    $selectedFile = (fd --type f --hidden --exclude .git --exclude .venv) | Invoke-Fzf
+    if ($selectedFile) {
+        $currentDirectory = (Get-Location).Path
+        $fullPathToOpen = Join-Path -Path $currentDirectory -ChildPath $selectedFile
+
+        $nvimCommand = "nvim `"$fullPathToOpen`""
+        Start-Process cmd.exe -ArgumentList "/c $nvimCommand" -NoNewWindow -Wait
+    }
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
 }
 
-# Initialize zoxide
-Invoke-Expression (& { (zoxide init powershell --cmd j | Out-String) })
-oh-my-posh prompt init pwsh --config C:\Users\nicol\.mytheme.omp.json | Invoke-Expression
-Write-Host "`e[6 q"
+  Set-PSReadLineKeyHandler -Key Ctrl+g -ScriptBlock {
+    Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+  }
+}
 
-
-
-
-
-
+Invoke-Expression (& { (zoxide init powershell --cmd j | Out-String) }) # Initialize zoxide
+# oh-my-posh prompt init pwsh --config C:\Users\nicol\.mytheme.omp.json | Invoke-Expression
