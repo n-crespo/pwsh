d695531ea1038ca3ee892712750cebefc73af444
Nicolas Crespo <nicolasecrespo@gmail.com>
2026-02-02 12:52:04 -0800

    chore: cleaning up to reduce startup time

--- profile.ps1
+++ profile.ps1
@@ -7,12 +7,12 @@ $env:PATH += ";C:\Users\nicol\AppData\Local\Programs\arduino-ide\resources\app\l
 $env:PATH += ";C:\Windows\System33"
 $env:ARDUINO_CONFIG_FILE = "C:\Users\nicol\.arduinoIDE\arduino-cli.yaml"
 $env:FZF_DEFAULT_OPTS="--style=minimal --info=inline --height=20% --reverse"
-# $env:_ZO_FZF_OPTS="--style=minimal --info=inline --height=20% --reverse"
 $env:FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git --exclude .venv"
+# $env:_ZO_FZF_OPTS="--style=minimal --info=inline --height=20% --reverse"
 # $PSModuleAutoLoadingPreference = 'None'
-Import-Module Microsoft.PowerShell.Utility
-Import-Module Microsoft.PowerShell.Management
-Import-Module PsFzf
+# Import-Module Microsoft.PowerShell.Utility
+# Import-Module Microsoft.PowerShell.Management
+# Import-Module PsFzf
 
 function lg { lazygit @args }
 function n { nvim @args }
@@ -31,74 +31,50 @@ function ltt { eza --tree @args }
 function cenv { py -m venv .venv }
 function senv { .venv\Scripts\activate }
 function o { fd --type f --exclude .git --hidden | Invoke-Fzf | ForEach-Object { nvim $_ } }
-function pk {
-  Invoke-FuzzyKillProcess
-  [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-}
 function e {
   $tmp = New-TemporaryFile
   yazi --cwd-file $tmp.FullName
   $cwd = Get-Content $tmp.FullName
-  if ($cwd -ne $PWD.Path) {
-    Set-Location $cwd
-  }
+  if ($cwd -ne $PWD.Path) { Set-Location $cwd }
   Remove-Item $tmp.FullName
 }
 function attackstart {
   try {
     vmrun -T ws start "C:\Users\nicol\OneDrive\Documents\Virtual Machines\Test VM\Test VM.vmx" nogui
-  } catch
-  { Write-Error "Failed to start the attack VM: $_"
-  }
+  } catch { Write-Error "Failed to start the attack VM: $_" }
 }
 function attackstop {
   try {
     vmrun -T ws stop "C:\Users\nicol\OneDrive\Documents\Virtual Machines\Test VM\Test VM.vmx" hard
-  } catch
-  { Write-Error "Failed to stop the attack VM: $_"
-  }
+  } catch { Write-Error "Failed to stop the attack VM: $_" }
 }
 function targetstart {
   try {
     vmrun -T ws start "C:\Users\nicol\OneDrive\Documents\Virtual Machines\Target\Target.vmx" nogui
-  } catch
-  { Write-Error "Failed to start the target VM: $_" }
+  } catch { Write-Error "Failed to start the target VM: $_" }
 }
 function targetstop {
   try {
     vmrun -T ws stop "C:\Users\nicol\OneDrive\Documents\Virtual Machines\Target\Target.vmx" hard
-  } catch
-  { Write-Error "Failed to stop the target VM: $_" }
+  } catch { Write-Error "Failed to stop the target VM: $_" }
 }
 function global:prompt {
-  $currentLocation = Get-Location
-  $currentDirectory = $currentLocation.Path
-  $displayPath = $currentDirectory.Replace($HOME, "~")
-  $windowTitle = "${displayPath}: "
+  # using automatic variable $pwd is faster than calling Get-Location
+  $displayPath = $pwd.Path -replace [regex]::Escape($HOME), "~"
 
-  # tell windows terminal the current working directory
-  if ($currentLocation.Provider.Name -eq "FileSystem") {
-    $cwdSequence = "$([char]27)]9;9;`"$($currentDirectory)`"$([char]27)\"
-    Write-Host $cwdSequence -NoNewline
+  # direct .NET write is faster than Write-Host for the OSC sequence
+  if ($pwd.Provider.Name -eq "FileSystem") {
+      [Console]::Write("$([char]27)]9;9;`"$($pwd.Path)`"$([char]27)\")
   }
 
+  # update window title
   if ($Host.Name -ne "ConsoleHost") {
-      $processName = $Host.Name
-  }
-  else {
-      $processName = (Get-Process -Id $PID).ProcessName
-      $windowTitle = "${displayPath}"
-  }
-
-  # to include the name of the host process even if it's the console, uncomment the line below:
-  # $Host.UI.RawUI.WindowTitle = "$windowTitle $processName"
-  if ($Host.Name -ne "ConsoleHost") {
-      $Host.UI.RawUI.WindowTitle = "${windowTitle}: $($Host.Name)"
+      $Host.UI.RawUI.WindowTitle = "$(displayPath): $($Host.Name)"
   } else {
-      $Host.UI.RawUI.WindowTitle = "${displayPath}"
+      $Host.UI.RawUI.WindowTitle = $displayPath
   }
 
-  Write-Host "[win] ${displayPath} ❯" -NoNewline
+  [Console]::Write("[win] $displayPath ❯")
   return " "
 }
 
@@ -112,42 +88,14 @@ Set-Alias ff fastfetch
 Set-Alias open start
 Set-Alias s sfsu
 
-if ($Host.UI.SupportsVirtualTerminal) {
-  # interactive shell only
-  # [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-  Import-Module PSReadLine
-  Set-PSReadLineOption -BellStyle None
-  Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
-  Set-PSReadLineKeyHandler -Key ctrl+d -Function ViExit
-  Set-PSReadLineKeyHandler -Key ctrl+n -Function NextHistory
-  Set-PSReadLineKeyHandler -Key ctrl+p -Function PreviousHistory
-  Set-PSReadLineKeyHandler -Key ctrl+l -Function ClearScreen
-  Set-PSReadLineKeyHandler -Key ctrl+enter -Function AcceptLine
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
-  Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { Invoke-FuzzyHistory }
-  Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-  Set-PSReadLineKeyHandler -Key Ctrl+j -ScriptBlock { zoxide query -l | Invoke-Fzf | Set-Location }
-
-  Set-PsFzfOption -TabExpansion
-  Set-PsFzfOption -EnableAliasFuzzyEdit # fe
-  Set-PsFzfOption -EnableAliasFuzzyKill # fkill
-  Set-PsFzfOption -EnableAliasFuzzyScoop # fs
-  Set-PsFzfOption -EnableAliasPsFzfRipGrep
-  # Set-PsFzfOption -EnableAliasFuzzyEdit # pk
-
-  # Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-  # Set-PSReadLineOption -PredictionViewStyle Inline
-
-  # Set-PSReadLineKeyHandler -Key Ctrl+g -ScriptBlock {
-  #   Get-ChildItem . -Depth 4 -Attributes Directory | Invoke-Fzf | Set-Location
-  # }
-
-  # open a file (i can't get this to work)
-  # Set-PSReadLineKeyHandler -Key Ctrl+o -ScriptBlock {
-    # Invoke-FuzzyEdit
-    # [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
-  # }
-}
+# if ($Host.UI.SupportsVirtualTerminal) {
+#   Import-Module PSReadLine
+#   Set-PSReadLineOption -BellStyle None
+#   Set-PSReadLineKeyHandler -Key Ctrl+r -ScriptBlock { Invoke-FuzzyHistory }
+#   Set-PSReadLineKeyHandler -Key Ctrl+j -ScriptBlock { zoxide query -l | Invoke-Fzf | Set-Location }
 
 Invoke-Expression (& { (zoxide init powershell --cmd j | Out-String) }) # Initialize zoxide
 Invoke-Expression (&sfsu hook)
